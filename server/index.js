4abaaf2564689bc7aeb551038fa8282ba690b117
Patea Tlau <pateatlau@gmail.com>
2025-03-30 20:53:37 +0530

    Temp additions

--- server/index.js
+++ server/index.js
@@ -7,6 +7,56 @@ import bodyParser from 'body-parser';
 import postRoutes from './routes/posts.js';
 
 dotenv.config();
+// TEMP STARTS
+// const arr1 = [
+//   [1, 2, 3],
+//   [4, 5],
+// ];
+// const arr2 = [0, [0.1, [[1, [1.1]], 2, 3], [4, 5], 6, 7], 8];
+// function flatten(arr, level = 1) {
+//   if (level === 0) return arr;
+//   return arr.reduce(function (flat, toFlatten) {
+//     return flat.concat(
+//       Array.isArray(toFlatten) ? flatten(toFlatten, level - 1) : toFlatten
+//     );
+//   }, []);
+// }
+
+// console.log(flatten(arr1, 0));
+// console.log(flatten(arr2, 0));
+import fs from 'fs';
+console.log('Start');
+
+setTimeout(() => {
+  console.log('Timeout 1');
+}, 700);
+
+setImmediate(() => {
+  console.log('Immediate 1');
+});
+
+process.nextTick(() => {
+  console.log('nextTick 1');
+});
+
+new Promise((resolve) => {
+  resolve('Resolved 1');
+}).then((res) => {
+  console.log(res);
+});
+
+setTimeout(() => {
+  console.log('Timeout 2');
+}, 300);
+
+fs.readFile('package.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log('File Read Complete:', data);
+});
+
+console.log('End');
+
+// TENP ENDS
 
 const app = express();
 
